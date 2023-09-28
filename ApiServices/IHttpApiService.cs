3e318a2f191889a8edff48857d032ba6303a726b
kanikmf <kanikkmf@gmail.com>
2023-09-28 17:21:08 +0300

    Add project files.

---
+++ ApiServices/IHttpApiService.cs
@@ -0,0 +1,13 @@
+﻿namespace InfinityPlatform.MvcUI.ApiServices
+{
+    public interface IHttpApiService
+  {
+     
+
+        Task<T> GetData<T>(string requestUri, string token = null);
+        Task<T> PostData<T>(string requestUri, string jsonData, string token = null);
+        Task<bool> DeleteData(string requestUri, string token = null);
+
+        Task<bool> UpdateData(string requestUri, string jsonData, string token = null);
+    }
+}
